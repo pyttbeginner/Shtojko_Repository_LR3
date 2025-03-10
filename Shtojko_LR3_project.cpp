550b50955d68f695048b702f29a262a9cfffc052
pyttbeginner <fomenckodimon783@yandex.ru>
2025-03-10 15:55:07 +0300

    void main func

--- Shtojko_LR3_project.cpp
+++ Shtojko_LR3_project.cpp
@@ -5,7 +5,9 @@ using namespace std
 
 double SizeBOutput()
 {
-
+string b;
+cin b;
+cout<<sizeOf(b);
 }
 
 double SizeBtoMB()
@@ -15,5 +17,27 @@ double SizeBtoMB()
 
 double SizeBtoGB()
 {
-    
+
+}
+
+void main()
+{
+cout<<"Lauch code? (1 - Yes, 0 - No)";
+cin>> bool yn;
+while (yn = 1){
+    switch(x)
+    {      
+    case 1:
+        SizeBOutput;
+        break
+    case 2:
+        SizeBtoMB;
+        break
+    case 3:
+        SizeBtoGB;
+        break
+    defaul:
+        None
+    }
+}
 }
